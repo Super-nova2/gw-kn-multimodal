d57872af420c11574f442544967fa218dd79b509
supernova <2876521732@qq.com>
2025-12-01 19:29:06 +1100

    rename

---
+++ dataset/KN_sim/snana_io.py
@@ -0,0 +1,8 @@
+# This file is part of ML+GW+KN, a package for machine learning applications in gravitational wave and kilonova data analysis.
+# The code is used to get photometry data from snlc_sim.exe results.
+
+import os
+import numpy as np
+import pandas as pd
+from astropy.time import Time
+
