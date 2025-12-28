862383c3fcf11aefecb42ce628b6934b63c40615
supernova <2876521732@qq.com>
2025-12-28 14:17:27 +1100

    Remove unused snana_io.py file and update kernel display name in KN_params_nsbh.ipynb to include version.

--- dataset/KN_sim/snana_io.py
+++
@@ -1,8 +0,0 @@
-# This file is part of ML+GW+KN, a package for machine learning applications in gravitational wave and kilonova data analysis.
-# The code is used to get photometry data from snlc_sim.exe results.
-
-import os
-import numpy as np
-import pandas as pd
-from astropy.time import Time
-
