18a231e7918db9c7857221a350ee592dafb2cef9
supernova <2876521732@qq.com>
2026-03-19 02:25:52 +1100

    feat: Add dataset handle management and loader configuration for improved memory management

--- Model/ALBEF_train.sh
+++ Model/ALBEF_train.sh
@@ -5,8 +5,8 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=220G
-#SBATCH --time=24:00:00
+#SBATCH --mem=64G
+#SBATCH --time=12:00:00
 #SBATCH --gres=gpu:1
 #SBATCH --partition=gpu
 #SBATCH --tmp=200G
